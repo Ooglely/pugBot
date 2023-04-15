a3dffaddaaf03792cee3da6ef4dae0356bb4e940
Ooglely <alennavdic@gmail.com>
2023-04-15 18:41:24 -0500

    Add constants file instead of using envs everywhere

---
+++ constants.py
@@ -0,0 +1,10 @@
+import json
+import os
+
+with open("config.json") as json_file:
+    config_file = json.load(json_file)
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN") or config_file["discord_token"]
+NEW_COMMIT_NAME = os.getenv("RAILWAY_GIT_COMMIT_SHA") or "Local Test"
+TESTING_GUILDS = config_file["testing_guild_ids"] or None
+DB_URL = os.getenv("DB_URL") or config_file["db_connection_string"]
