7aa86c2e5fcffae9b0580849b77039bbd057c6e5
Alen Avdic <aahgr@umsystem.edu>
2023-04-16 14:01:17 -0500

    Add version const

--- constants.py
+++ constants.py
@@ -8,3 +8,4 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN") or config_file["discord_token"]
 NEW_COMMIT_NAME = os.getenv("RAILWAY_GIT_COMMIT_SHA") or "Local Test"
 TESTING_GUILDS = config_file["testing_guild_ids"] or None
 DB_URL = os.getenv("DB_URL") or config_file["db_connection_string"]
+VERSION = config_file["version"] or "dev"
