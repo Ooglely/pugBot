1fbc0aba93f6d021aa064463dded4ac5eadd5027
Ooglely <alennavdic@gmail.com>
2023-06-12 15:48:40 -0500

    Use correct db url

--- constants.py
+++ constants.py
@@ -12,7 +12,7 @@ else:
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN") or config_file["discord_token"]
 NEW_COMMIT_NAME = os.getenv("RAILWAY_GIT_COMMIT_SHA") or "Local Test"
 TESTING_GUILDS = config_file["testing_guild_ids"] or None
-DB_URL = os.getenv("DB_URL") or config_file["db_connection_string"]
+DB_URL = os.getenv("MONGO_URL") or config_file["db_connection_string"]
 VERSION = os.getenv("BOT_VERSION") or config_file["version"] or "dev"
 API_PASSWORD = os.getenv("BOT_API_PASSWORD") or None
 PORT = os.getenv("PORT") or 8000
